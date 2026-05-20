f88d13430c567dca7d2fc5af8a4d1c242de06684
almeida2 <projetodidaticovl@gmail.com>
2026-05-20 16:56:29 -0300

    test: ajuste na nomenclatura da configuração do teste de integração

--- src/test/java/com/fatec/sigvs_produto/service/TIMantemProdutoServiceTests.java
+++ src/test/java/com/fatec/sigvs_produto/service/TIMantemProdutoServiceTests.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class ProdutoServiceIntegrationTest {
+public class TIMantemProdutoServiceTests {
 
     @Autowired
     private IProdutoService service;
